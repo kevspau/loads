16a58781910257801a0a1fafc135debbfa92869f
csharpdf <csharpdfjer@gmail.com>
2022-02-03 18:18:49 +0000

    h

---
+++ Main.hx
@@ -0,0 +1,6 @@
+class Main {
+    final FGBlack, FGRed, FGGreen, FGYellow, FGBlue, FGMagenta, FGCyan, FGWhite, BGBlack, BGRed, BGGreen, BGYellow, BGBlue, BGMagenta, BGCyan, BGWhite = 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107;
+    public static function main() {
+
+    }
+}
